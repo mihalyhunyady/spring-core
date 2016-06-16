4d34ccde6eb3dddd9a5337d72df2779f9b2a5d6a
Mihály Hunyady <megamegax@gmail.com>
2016-06-16 13:50:41 +0200

    task

--- src/main/java/com/epam/botor/App.java
+++ src/main/java/com/epam/botor/App.java
@@ -8,8 +8,8 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.epam.botor.domain.Army;
+import com.epam.botor.domain.Battle;
 import com.epam.botor.domain.BlasterPistol;
-import com.epam.botor.domain.FighterType;
 import com.epam.botor.domain.Jedi;
 import com.epam.botor.domain.Person;
 import com.epam.botor.domain.Trooper;
@@ -19,46 +19,50 @@ import com.epam.botor.domain.Trooper;
  *
  */
 public class App {
-	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
-	
-	public static void main(String[] args) {
-		
-		try (AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
-			Jedi jedi = context.getBean("odanUrr", Jedi.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-			LOGGER.debug(jedi.fight());
-			
-			LOGGER.debug("" + jedi.getIdentityHashCode());
-		
-			Trooper trooper = context.getBean("trooper", Trooper.class);
-			LOGGER.debug(trooper.getName() + ": " + trooper.getIdentityHashCode());
-			trooper = context.getBean("trooper", Trooper.class);
-			LOGGER.debug(trooper.getName() + ": " + trooper.getIdentityHashCode());
-			
-			trooper = context.getBean("trooper2", Trooper.class);
-			LOGGER.debug(trooper.getName() + ": " + trooper.getIdentityHashCode());
-		
-			LOGGER.debug("" + context.getBean("odanUrrLightSabre"));
-			
-			
-			LOGGER.debug("Total memory: " + context.getBean("runtime", Runtime.class).totalMemory());
-			LOGGER.debug("" + context.getBean("johan"));
-			List<Person> fighters = context.getBean("lightArmy", Army.class).getFighters();
-			LOGGER.debug("" + fighters + " " + fighters.getClass());
-			LOGGER.debug("" + (fighters.get(0) == fighters.get(1)));
-			
-			LOGGER.debug("" + context.getBean("trooper3"));
-			LOGGER.debug("" + context.getBean("odanUrr", Jedi.class));
-			
-			LOGGER.debug("" + context.getBean("defaultLightSabre"));
-			LOGGER.debug("" + context.getBean("ageOfTrooper3"));
-			LOGGER.debug("" + context.getBean("obi"));
-			
-			LOGGER.debug("" + context.getBean(BlasterPistol.class));
-			LOGGER.debug("" + context.getBean(BlasterPistol.class));
-			LOGGER.debug("" + context.getBean(BlasterPistol.class));
-			LOGGER.debug("" + context.getBean("mace"));
-			LOGGER.debug("" + context.getBean("lightArmy"));
-		}
-	}
+    public static void main(String[] args) {
+
+        try (AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+            Jedi jedi = context.getBean("odanUrr", Jedi.class);
+
+            LOGGER.debug(jedi.fight());
+
+            LOGGER.debug("" + jedi.getIdentityHashCode());
+
+            Trooper trooper = context.getBean("trooper", Trooper.class);
+            LOGGER.debug(trooper.getName() + ": " + trooper.getIdentityHashCode());
+            trooper = context.getBean("trooper", Trooper.class);
+            LOGGER.debug(trooper.getName() + ": " + trooper.getIdentityHashCode());
+
+            LOGGER.debug(trooper.getName() + ": " + trooper.getIdentityHashCode());
+
+            LOGGER.debug("" + context.getBean("odanUrrLightSabre"));
+
+            LOGGER.debug("Total memory: " + context.getBean("runtime", Runtime.class).totalMemory());
+            LOGGER.debug("" + context.getBean("johan"));
+            List<Person> fighters = context.getBean("lightArmy", Army.class).getFighters();
+            LOGGER.debug("" + fighters + " " + fighters.getClass());
+            LOGGER.debug("" + (fighters.get(0) == fighters.get(1)));
+
+            LOGGER.debug("" + context.getBean("odanUrr", Jedi.class));
+
+            LOGGER.debug("" + context.getBean("defaultLightSabre"));
+            LOGGER.debug("" + context.getBean("ageOfTrooper"));
+            LOGGER.debug("" + context.getBean("obi"));
+
+            LOGGER.debug("" + context.getBean(BlasterPistol.class));
+            LOGGER.debug("" + context.getBean(BlasterPistol.class));
+            LOGGER.debug("" + context.getBean(BlasterPistol.class));
+            LOGGER.debug("" + context.getBean("mace"));
+            LOGGER.debug("" + context.getBean("lightArmy"));
+            LOGGER.debug("" + context.getBean("darkArmy"));
+
+            LOGGER.debug("" + ("------------------------"));
+            Battle battle = context.getBean("battle", Battle.class);
+            LOGGER.debug("" + battle.play());
+            LOGGER.debug("" + ("------------------------"));
+
+        }
+    }
 }
---
+++ src/main/java/com/epam/botor/StringTrimmer.java
@@ -0,0 +1,9 @@
+package com.epam.botor;
+
+public class StringTrimmer extends org.springframework.beans.propertyeditors.StringTrimmerEditor {
+
+    public StringTrimmer() {
+        super(false);
+    }
+
+}
